db25c6e67c52934b55e4a042d7124e284ce9d2b6
HishamFouda <aldawliksa@gmail.com>
2020-05-13 14:39:42 +0300

    - last edit

--- database/migrations/2020_03_29_185509_create_co_account_subscription_loggers_table.php
+++ database/migrations/2020_03_29_185509_create_co_account_subscription_loggers_table.php
@@ -29,8 +29,8 @@ class CreateCoAccountSubscriptionLoggersTable extends Migration
 
         Schema::table('co_account_subscription_loggers', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
-            $table->foreign('account_id')->references('id')->on('co_account_subscriptions')->onDelete('cascade');
-            $table->foreign('subscription_id')->references('id')->on('co_accounts')->onDelete('cascade');
+            $table->foreign('account_id')->references('id')->on('co_accounts')->onDelete('cascade');
+            $table->foreign('subscription_id')->references('id')->on('co_account_subscriptions')->onDelete('cascade');
         });
     }
 
