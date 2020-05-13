f962bf9a6142ba4be05360a6c957e5cab808ba95
HishamFouda <aldawliksa@gmail.com>
2020-05-13 14:00:21 +0300

    - update

--- app/CoAccountSubscriptionLogger.php
+++ app/CoAccountSubscriptionLogger.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CoAccountSubscriptionLogger extends Model
 {
-    protected $fillable = [ 'subscription_id', 'note', 'account_id', 'devices', 'period', 'type', 'created_at' ];
+    protected $fillable = [ 'user_id', 'subscription_id', 'note', 'account_id', 'devices', 'period', 'type', 'created_at' ];
 
     protected $dates = ['created_at'];
 
