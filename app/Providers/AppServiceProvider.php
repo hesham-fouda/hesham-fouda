307a3a8c03c3cfdc422654100eeea9a9f048f78a
HishamFouda <aldawliksa@gmail.com>
2020-05-13 16:18:50 +0300

    - fix audit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use OwenIt\Auditing\Models\Audit;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -27,13 +28,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //date_default_timezone_set('Africa/cairo');
-
-        App::singleton('user_timezone', function(){
+        /*App::singleton('user_timezone', function(){
             return 'Africa/cairo';
-        });
+        });*/
 
         Validator::extend('eg_phone_number', function($attribute, $value, $parameters, $validator){
             return preg_match('/^(00201|201|\+201|01)(0|1|2|5)([0-9]{8})$/', $value);
+        });
+
+        Audit::creating(function (Audit $model) {
+            if (empty($model->old_values) && empty($model->new_values)) {
+                return false;
+            }
         });
     }
 }
